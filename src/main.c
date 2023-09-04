e74783b15ba4e53fd7abab895e42df1d29f4d18a
Rhett Garber <rhettg@gmail.com>
2023-09-04 16:45:53 -0700

    camera init

--- src/main.c
+++ src/main.c
@@ -395,6 +395,7 @@ static camera_config_t camera_config = {
 
     .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
     .fb_count = 1, //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+    .fb_location = CAMERA_FB_IN_PSRAM, //Choose the location of frame buffer
     .grab_mode = CAMERA_GRAB_WHEN_EMPTY//CAMERA_GRAB_LATEST. Sets when buffers should be filled
 };
 
